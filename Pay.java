3cbd93938178ffa83ad7e9edf46e93fdb7d0ef1a
KobzaS <32111227+KobzaS@users.noreply.github.com>
2017-10-10 13:31:00 -0700

    Adding the code

--- Pay.java
+++ Pay.java
@@ -1,4 +1,61 @@
 class Pay
 {
-    
+    private float hours, rate;
+    private int hrsStart;
+    private double grossPay, tax, grossPayCalc;
+    public double calc_PayRoll()
+    {
+        if (hrsStart = 0)
+        {
+            grossPay = hours*rate;
+        }
+        
+        if (hours > hrsStart)
+        {
+            grossPayCalc = hours - hrsStart;
+            hours = hrsStart;
+            grossPay = hours * rate;
+            grossPay += grossPayCalc * (rate*1.33);
+        }
+        else 
+        {
+            grossPay = hours * rate;
+        }
+        return grossPay;
+    }
+    public double tax()
+    {
+        if (grossPay < 400 && grossPay >= 0)
+        tax = 8;
+        else if (grossPay < 900 && grossPay >= 400)
+        tax = 12;
+        else if (grossPay >= 900)
+        tax = 16;
+
+        return tax;
+    }
+    public void setHours (float h)
+    {
+        hours = h;
+    }
+    public void setRate(float r)
+    {
+        rate = r;
+    }
+    public void setHrsStr(int hs)
+    {
+        hrsStart = hs;
+    }
+    public float getHours()
+    {
+        return hours;
+    }
+    public float getRate()
+    {
+        return rate;
+    }
+    public int getHrsStr()
+    {
+        return hrsStart;
+    }
 }
