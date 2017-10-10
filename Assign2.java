d218807b7a5720c7aaeaf99b423cefd73992a577
dmennie <32312277+dmennie@users.noreply.github.com>
2017-10-10 13:31:41 -0700

    WhateverIwant

--- Assign2.java
+++ Assign2.java
@@ -1,4 +1,24 @@
 class Assign2
 {
 
+   public static void main(String[] args)
+   {
+      CalPayroll cal = new CalPayroll();
+      Accept ac1 = new Accept();
+      Screen sc1 = new Screen();
+      
+      cal.acceptPay();  
+      
+      System.out.print("Press 'e' to exit or any other leter + Enter to continue: ");
+      char choice = ac1.AcceptInputChar();
+      if (choice == 'e' || choice == 'E')
+      {
+         System.exit(0);
+      }
+      else
+      {
+         sc1.scrollScreen(45);
+         cal.acceptPay();
+      }
+   }
 }
